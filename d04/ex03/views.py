63b09a753fcc76835270c1697957cf1b24ec4086
Marie Dereuse <mdereuse@student.42.fr>
2024-02-19 01:30:08 +0100

    ex03

--- d04/ex03/views.py
+++ d04/ex03/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-    color_range =  [
-            for i in range(5, 255, 5)
+    black_color_range =  [
+        f"#{i:02X}{i:02X}{i:02X}" for i in range(5, 255, 5)
+    ]
+    red_color_range =  [
+        f"#{i:02X}0000" for i in range(5, 255, 5)
+    ]
+    blue_color_range =  [
+        f"#0000{i:02X}" for i in range(5, 255, 5)
+    ]
+    green_color_range =  [
+        f"#00{i:02X}00" for i in range(5, 255, 5)
     ]
     context = {
-        "color_range": color_range
+        "color_range": zip(black_color_range, red_color_range, blue_color_range, green_color_range)
     }
+    print(context)
     return render(request, 'ex03/index.html', context)
